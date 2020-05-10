2168473ce07946af642fbb7690f4817adeefa5fe
Damir Mubarakshin <DRMubarakshin@sberbank.ru>
2020-05-10 23:13:49 +0300

    Add calendar by clicking the button

--- app/src/main/java/com/example/grid/MainActivity.kt
+++ app/src/main/java/com/example/grid/MainActivity.kt
@@ -1,18 +1,36 @@
 package com.example.grid
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuInflater
-import android.widget.Toolbar
-import kotlinx.android.synthetic.*
+import android.view.View
+import android.widget.CalendarView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_header.*
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // calendar
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
+
+        // button click to show DatePickerDialog
+        calendarButton.setOnClickListener {
+            val dpd = DatePickerDialog(this, DatePickerDialog.OnDateSetListener{view, year, month, dayOfMonth ->
+                // some action
+            }, year, month, day)
+            // show dialog
+            dpd.show()
+        }
+
     }
 
     private val day = arrayOf("Mon", "Tue", "Wen", "Thu", "Fri")
@@ -21,4 +39,8 @@ class MainActivity : AppCompatActivity() {
         super.onWindowFocusChanged(hasFocus)
         table.initTable(day)
     }
+
+    // calendar
+
+
 }
