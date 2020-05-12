d2d1708921cee6ac09e734388ba67980ffe2b78a
Damir Mubarakshin <DRMubarakshin@sberbank.ru>
2020-05-12 23:56:39 +0300

    Correct strings and functions

--- app/src/main/java/com/example/grid/CreateProjectActivity.kt
+++ app/src/main/java/com/example/grid/CreateProjectActivity.kt
@@ -2,6 +2,7 @@ package com.example.grid
 
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 class CreateProjectActivity : AppCompatActivity() {
@@ -17,5 +18,15 @@ class CreateProjectActivity : AppCompatActivity() {
 
     fun onCreateButtonClick(view: View) {
         // input data processing
+        // input values
+        val projectTitle = findViewById<EditText>(R.id.create_project_title_input).text.toString()
+        val projectDescription = findViewById<EditText>(R.id.create_project_description_input).text.toString()
+        val projectStartDate = findViewById<EditText>(R.id.create_project_start_date_input).text.toString()
+        val projectEndDate = findViewById<EditText>(R.id.create_project_end_date_input).text.toString()
+        val projectColor = findViewById<EditText>(R.id.create_project_color_input).text.toString()
+    }
+
+    fun onChooseColorButtonClick(view: View) {
+
     }
 }
