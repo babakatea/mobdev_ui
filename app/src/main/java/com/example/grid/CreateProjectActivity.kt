567d51366f2d94ad40bddcb4d7626ea05a0cbe27
Damir Mubarakshin <DRMubarakshin@sberbank.ru>
2020-05-13 16:03:04 +0300

    Date pickers

--- app/src/main/java/com/example/grid/CreateProjectActivity.kt
+++ app/src/main/java/com/example/grid/CreateProjectActivity.kt
@@ -1,15 +1,53 @@
 package com.example.grid
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.project_create.*
+import java.util.*
 
 class CreateProjectActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.project_create)
+    }
+
+    fun onChooseStartDateButtonClick(view: View) {
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
+
+
+        val dpd = DatePickerDialog(
+            this,
+            DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+                val startDateText = findViewById<TextView>(R.id.create_project_start_date_text)
+                    .setText(dayOfMonth.toString() + "." + monthOfYear + "." + year)
+            }, year, month, day
+        )
+        dpd.show()
+    }
+
+    fun onChooseEndDateButtonClick(view: View) {
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
+
+
+        val dpd = DatePickerDialog(
+            this,
+            DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+                val startDateText = findViewById<TextView>(R.id.create_project_end_date_text)
+                    .setText(dayOfMonth.toString() + "." + monthOfYear + "." + year)
+            }, year, month, day
+        )
+        dpd.show()
     }
 
     fun onCancelButtonClick(view: View) {
@@ -20,10 +58,11 @@ class CreateProjectActivity : AppCompatActivity() {
         // input data processing
         // input values
         val projectTitle = findViewById<EditText>(R.id.create_project_title_input).text.toString()
-        val projectDescription = findViewById<EditText>(R.id.create_project_description_input).text.toString()
-        val projectStartDate = findViewById<EditText>(R.id.create_project_start_date_input).text.toString()
-        val projectEndDate = findViewById<EditText>(R.id.create_project_end_date_input).text.toString()
-        val projectColor = findViewById<EditText>(R.id.create_project_color_input).text.toString()
+        val projectDescription =
+            findViewById<EditText>(R.id.create_project_description_input).text.toString()
+//        val projectStartDate = findViewById<EditText>(R.id.create_project_start_date_input).text.toString()
+//        val projectEndDate = findViewById<EditText>(R.id.create_project_end_date_input).text.toString()
+//        val projectColor = findViewById<EditText>(R.id.create_project_color_input).text.toString()
     }
 
     fun onChooseColorButtonClick(view: View) {
