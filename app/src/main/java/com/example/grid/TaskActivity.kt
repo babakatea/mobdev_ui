0b6dce55ababa0dab2769d9ae5847cf029656a41
ecaterina baba <ecaterina@cubedmobile.com>
2020-05-13 03:32:42 +0300

    ADD task edit view

--- app/src/main/java/com/example/grid/TaskActivity.kt
+++ app/src/main/java/com/example/grid/TaskActivity.kt
@@ -2,12 +2,9 @@ package com.example.grid
 
 import android.app.DatePickerDialog
 import android.os.Bundle
-import android.view.View
-import android.widget.DatePicker
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_task.*
 import java.util.Calendar
-import javax.xml.datatype.DatatypeConstants.MONTHS
 
 class TaskActivity : AppCompatActivity() {
 
@@ -15,21 +12,34 @@ class TaskActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_task)
 
-        start_date.setOnClickListener(object : View.OnClickListener {
-            override fun onClick(v: View?) {
-                val calendar = Calendar.getInstance()
-                val year = calendar.get(Calendar.YEAR)
-                val month = calendar.get(Calendar.MONTH)
-                val day = calendar.get(Calendar.DAY_OF_MONTH)
+        start_date.setOnClickListener {
+            val calendar = Calendar.getInstance()
+            val year = calendar.get(Calendar.YEAR)
+            val month = calendar.get(Calendar.MONTH)
+            val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+            DatePickerDialog(
+                this@TaskActivity,
+                DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+                    start_date.setText("$dayOfMonth/$monthOfYear/$year")
+                }, year, month, day
+            ).show()
+        }
+
+        end_date.setOnClickListener {
+            val calendar = Calendar.getInstance()
+            val year = calendar.get(Calendar.YEAR)
+            val month = calendar.get(Calendar.MONTH)
+            val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+            DatePickerDialog(
+                this@TaskActivity,
+                DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+                    end_date.setText("$dayOfMonth/$monthOfYear/$year")
+                }, year, month, day
+            ).show()
+        }
 
 
-                DatePickerDialog(this@TaskActivity,
-                    DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
-                        start_date.setText("$dayOfMonth/$monthOfYear/$year")
-                    }, year, month, day
-                ).show()
-
-            }
-        })
     }
 }
